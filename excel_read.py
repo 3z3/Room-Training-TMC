cc36fff6f866df8ff0ab3f855bb3a484fffa3201
incommunicado <adriandu31@gmail.com>
2025-05-09 03:14:39 +0200

    Update excel_read.py

--- excel_read.py
+++ excel_read.py
@@ -1,20 +1,15 @@
 import openpyxl
 
-wb = openpyxl.load_workbook('..\\TMCroomlist.xlsx')
+wb = openpyxl.load_workbook('..\\TMCroomlist.xlsx')    #path to the excel file being read, update depending on how you organize your files
 
 ws = wb.active
 
 memory_to_rooms = {}
 memory_to_areas = {}
 
-#example
-#ws['B3'].value is None
-#ws.cell(row=1,column=i).value iterates through columns using index i
-#ws.max_column designates the highest column number
-
 for i in range(2,ws.max_row+1):
     area_cell = ws.cell(row = i, column = 2)
-    if area_cell.value is None or area_cell.fill.bgColor.rgb != 'FFFFFFFF':
+    if area_cell.value is None or area_cell.fill.bgColor.rgb != 'FFFFFFFF':    #cells that are not white are bugged rooms or rooms that are not used in the game
         pass
     else:
         if area_cell.value not in memory_to_rooms:
@@ -29,10 +24,20 @@ for i in range(2,ws.max_row+1):
 mtrstring = 'memory_to_rooms = {'
 mtastring = 'memory_to_areas = {'
 
+#block needed since some areas have the same name on the excel file but different memory addresses
+area_individual_names = {}
+
+for memory,name in memory_to_areas.items():
+    area_individual_names[name] = area_individual_names.get(name,0) + 1
+    if area_individual_names[name] > 1:
+        memory_to_areas[memory] = name + " " + string(area_individual_names[name])    #count, eg. "Dark Hyrule Castle 2"
+
+#translates the memory_to_rooms dictionary into Lua
 for key,value in memory_to_rooms.items():
     mtrstring += ' ["' + key + '"] = "' + value + '",'
 mtrstring[:-1] += ' }\n'    #getting rid of the last comma at the end
 
+#translates the memory_to_areas dictionary into Lua
 for key,value in memory_to_areas.items():
     mtastring += ' ["' + key + '"] = "' + value + '",'
 mtastring[:-1] += ' }'
