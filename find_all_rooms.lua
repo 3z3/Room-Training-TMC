8921819686d28eae516142d9432963817944ac8b
incommunicado <adriandu31@gmail.com>
2025-05-09 03:22:55 +0200

    Update and rename find_all_rooms.Lua to find_all_rooms.lua

--- find_all_rooms.lua
+++ find_all_rooms.lua
@@ -1,9 +1,13 @@
-local savestatepath = '..\\GBA\\State\\TESTFOLDER\\'
+local savestatepath = '..\\GBA\\State\\ROOMTRAINERFOLDER\\'    -- create a folder named ROOMTRAINERFOLDER in the State folder of Bizhawk beforehand
 local states_table = {}
+
+-- /!\ this script is designed to run while a movie of a TMC category full run plays back
 
 local current_roomandarea = 0x0
 local previous_roomandarea = memory.read_u16_le(0x10AC,"IWRAM")
 
+-- uncomment this line if you want to replay your movie at 500% speed so that it does not take as long
+-- client.speedmode(500)
 
 while memory.read_u8(0x1744,"IWRAM") ~= 0x0D do -- vaati 3 is dead memory value
 
@@ -16,7 +20,7 @@ while memory.read_u8(0x1744,"IWRAM") ~= 0x0D do -- vaati 3 is dead memory value
         end
         local count = 0
         if states_table[temp_key] == nil then
-            states_table[temp_key] = {'state0'}
+            states_table[temp_key] = {'state0'}    -- states are going to be saved according to the following syntax : 0188state0.State for example is the first savestate of the Top Left Corner Key Chest room of Dark Hyrule Castle
         else
             for _,_ in ipairs(states_table[temp_key]) do
                 count = count + 1
