4da6098ffa645ce4a89a99ae9bbb4e24540d8f0e
incommunicado <adriandu31@gmail.com>
2025-05-10 15:34:46 +0200

    Update find_all_rooms.lua

--- find_all_rooms.lua
+++ find_all_rooms.lua
@@ -1,12 +1,14 @@
+RMNM = require "roomnames"   -- Lua file containing tables that translate memory addresses to names
 local savestatepath = '..\\GBA\\State\\ROOMTRAINERFOLDER\\'    -- create a folder named ROOMTRAINERFOLDER in the State folder of Bizhawk beforehand
 local states_table = {}
+local ordered_rooms = {}
 
 -- /!\ this script is designed to run while a movie of a TMC category full run plays back
 
 local current_roomandarea = 0x0
 local previous_roomandarea = memory.read_u16_le(0x10AC,"IWRAM")
 
--- uncomment this line if you want to replay your movie at 500% speed so that it does not take as long
+-- uncomment this line if you want to replay your movie at 500% speed (300 fps) so that it does not take as long
 -- client.speedmode(500)
 
 while memory.read_u8(0x1744,"IWRAM") ~= 0x0D do -- vaati 3 is dead memory value
@@ -28,6 +30,7 @@ while memory.read_u8(0x1744,"IWRAM") ~= 0x0D do -- vaati 3 is dead memory value
             states_table[temp_key][count+1] = 'state' .. string.format(count)
         end
         savestate.save(savestatepath .. temp_key .. 'state' .. string.format(count) .. '.State')    -- saving the savestate with the appropriate name
+        table.insert(ordered_rooms,RMNM.mem_to_areas(string.upper(string.sub(temp_key,3,4))) .. " # " .. RMNM.mem_to_rooms(string.upper(string.sub(temp_key,1,4))) .. " # " .. tostring(count))
     end
 
     previous_roomandarea = current_roomandarea
@@ -36,3 +39,14 @@ while memory.read_u8(0x1744,"IWRAM") ~= 0x0D do -- vaati 3 is dead memory value
 end
 
 -- ran once
+
+local orderedrooms_string = "local ORDERED_ROOMS = { "
+
+for _,name in ipairs(ordered_rooms) do
+    orderedrooms_string = orderedrooms_string .. '"' .. name .. '", '    -- be very careful : for example at some point an area is called Fortress of Winds "Outside", this was replaced with parentheses instead of quotations marks
+end
+orderedrooms_string = string.sub(orderedrooms_string,1,-3) .. " }"
+
+local newfile = io.open ("orderedrooms.lua", "w")
+newfile:write(orderedrooms_string)
+newfile:close()
