e38cdf10fb977356d66f2b273e18c52f8a5bc744
incommunicado <adriandu31@gmail.com>
2025-05-10 15:26:04 +0200

    Update room_training.lua

--- room_training.lua
+++ room_training.lua
@@ -1,9 +1,11 @@
 RMNM = require "roomnames"   -- Lua file containing tables that translate memory addresses to names
+ORM = require "orderedrooms"    -- Lua file containing every room visited in any% in order
 local statepath = '..\\GBA\\State\\ROOMTRAINERFOLDER\\'    -- append the name of a savestate file to the path for it to load properly with savestate.load()
 local path = '..\\GBA\\State\\'    -- path to State folder in which we save the temporary save for the room-only mode
 local temp_save = 'temporary_savestate.State'
 
 local timer_frames = 0
+local menu_time = 0
 
 local name_to_area = {}
 local name_to_room = {}
@@ -55,6 +57,19 @@ local function load_current_area()
     forms.setdropdownitems(ROOM_MENU,res_rooms)
 end
 
+local function load_ordered()
+    local splitted = string.gmatch(forms.gettext(ROOMS_IN_ORDER),"([^#]+)")
+    local area = string.sub(splitted(1),1,-2)
+    local room = string.sub(splitted(2),2,-2)
+    local multiple = string.sub(splitted(3),2)
+    local area_memory = name_to_area[area]
+    local room_memory = name_to_room[room .. "PLUS" .. area]
+    local res_savestate = savestate_names[area_memory][room_memory][tonumber(multiple)+1]
+    savestate.load(statepath .. res_savestate)
+    savestate.save(path .. temp_save)
+    timer_frames = 0
+end
+
 -- window
 FORM = forms.newform(300,200, "ROOM TRAINER")
 
@@ -72,6 +87,14 @@ LOAD_BUTTON = forms.button(FORM,"Load",load_current_state,10,100,100,20)
 ROOM_PRACTICE_CHECK = forms.checkbox(FORM,"Room-Only Mode",10,130)
 TIMER_CHECK = forms.checkbox(FORM,"Show Timer",10,160)
 SHUFFLE_RNG = forms.checkbox(FORM,"Shuffle RNG",120,130)
+MENU_TIME = forms.checkbox(FORM,"Time in Menu",120,160)
+
+-- drop down menu of all the rooms in order in the route
+forms.label(FORM,"Every Room in Order:",10,200,150,15)
+ROOMS_IN_ORDER = forms.dropdown(FORM,{"placeholder"},10,220,300,20)
+LOAD_ORDER = forms.button(FORM,"Load",load_ordered,10,250,100,20)
+
+forms.setdropdownitems(ROOMS_IN_ORDER,ORM.get_rooms(),false)    -- this keeps the saves in order
 
 local current_roomandarea = 0x0
 local previous_roomandarea = memory.read_u16_le(0x10AC,"IWRAM")
@@ -84,6 +107,7 @@ while true do
     if current_roomandarea ~= previous_roomandarea and forms.ischecked(ROOM_PRACTICE_CHECK) then
         savestate.load(path .. temp_save)
         timer_frames = 0
+        menu_time = 0
         if forms.ischecked(SHUFFLE_RNG) then
             -- thankfully, it just so happens that time is less than 2^32 so is a very good placeholder for a random rng value
             -- nevertheless, time may not be the best since counting rng up to the value obtained may take 2^31 tries on average, that is 2^31 evaluation of the rng function, which is very time consuming and will make bizhawk crash
@@ -93,18 +117,27 @@ while true do
     elseif current_roomandarea ~= previous_roomandarea then
         savestate.save(path .. temp_save)
         timer_frames = 0
+        menu_time = 0
         if forms.ischecked(SHUFFLE_RNG) then
             memory.write_u32_le(0x1150,os.time(),"IWRAM")
 
         end
     end
 
-    if forms.ischecked(TIMER_CHECK) then    -- displays the timer for a specific room since you entered it in frames and seconds
-        gui.drawText(150,5,tostring(timer_frames) .. " f","cyan","black",12)
-        gui.drawText(150,20,tostring((timer_frames//6)/10) .. " sec","pink","black",12)
+    if forms.ischecked(TIMER_CHECK) then
+        gui.drawText(175,30,tostring(timer_frames) .. " f","cyan","black",12)
+        gui.drawText(175,45,tostring((timer_frames//6)/10) .. " sec","pink","black",12)
     else
         gui.clearGraphics()
     end
+
+    if forms.ischecked(MENU_TIME) then    -- displays the timer for a specific room since you entered it in frames and seconds
+        if memory.read_u16_le(0x10CF,"IWRAM") == 0x0100 then
+            menu_time = menu_time + 1  
+        end
+        gui.drawText(160,137,tostring(menu_time) .. " f : menu","lime","black",10)
+    end
+
     previous_roomandarea = current_roomandarea
 
     emu.frameadvance()
